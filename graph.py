9ca446061e50de8b25b8aab087487318d6d237d3
Jakub Klapacz <kubaklapacz@gmail.com>
2016-11-02 16:54:43 -0500

    Added evaluation function

--- graph.py
+++ graph.py
@@ -16,6 +16,60 @@ def detect_communities(G):
     best = community.best_partition(G)
     return best
 
+def getTrue(filename):
+    true = []
+    with open(filename, 'r') as f:
+        for line in f:
+            circle = []
+            x = line.rstrip().split()
+            for node in x[1:]:
+                circle.append(int(node))
+            true.append(np.array(circle))
+
+def evaluate_score(true, predicted):
+    trueSize = float(len(true))
+    predSize = float(len(predicted))
+    p1 = 0.
+    p2 = 0.
+    for cStar in true:
+        cStar = np.array(cStar)
+        tempArr = []
+        for c in predicted:
+            cS = cStar
+            cC = c
+            if(len(c) > len(cStar)):
+                cS = np.array([-1] * len(c))
+                cS[:len(cStar)] = cStar
+#                 print("RESHAPING")
+            if(len(cStar) > len(c)):
+                cC = np.array([-1] * len(cStar))
+                cC[:len(c)] = c
+#                 print("RESHAPING")
+#             print("SHAPES: ", cS.shape, cC.shape)
+            tempArr.append(delta(cS, cC))
+        p1 += np.max(tempArr)
+    for cPred in predicted:
+        cPred = np.array(cPred)
+        tempArr = []
+        for cStar in true:
+            cS = cStar
+            cC = cPred
+            if(len(cPred) > len(cStar)):
+                cS = np.array([-1] * len(cPred))
+                cS[:len(cStar)] = cStar
+#                 print("RESHAPING")
+            if(len(cStar) > len(cPred)):
+                cC = np.array([-1] * len(cStar))
+                cC[:len(cPred)] = cPred
+#                 print("RESHAPING")
+#             print("SHAPES: ", cS.shape, cC.shape)
+            tempArr.append(delta(cS, cC))
+        p2 += np.max(tempArr)
+
+#     p2 += np.max([delta(cStar, cC) for cStar in true])
+    score = (1/(2*trueSize))*p1 + (1/(2*predSize))*p2
+    return score
+
 def run(filename):
     print('graph setup')
     G = setup_graph(filename)
